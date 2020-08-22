13fa556d4c1a6a6dcc8d54803ff25c03796b9c54
alfieqashwa <alfieqashwa@gmail.com>
2020-08-22 23:25:08 +0700

    everyday is a happy day

---
+++ interfaces/data.ts
@@ -0,0 +1,4 @@
+export type Data = {
+  name: string;
+  gender: string;
+};
---
+++ pages/api/cello.ts
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from "next";
+import { Data } from "interfaces/data";
+
+export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  res.statusCode = 200;
+  res.json({ name: "Muhammad Cello el-Zhafran", gender: "boy" });
+};
